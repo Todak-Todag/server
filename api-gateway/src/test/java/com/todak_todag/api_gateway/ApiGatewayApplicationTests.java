06978bfea40151a402941868af6a6dcbcd914a3d
Seo-JS0823 <seong0093571@naver.com>
2026-09-02 02:01:09 +0900

    fix: Local 환경 의존성 제거

--- api-gateway/src/test/java/com/todak_todag/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/todak_todag/api_gateway/ApiGatewayApplicationTests.java
@@ -2,10 +2,8 @@ package com.todak_todag.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class ApiGatewayApplicationTests {
 
 	@Test
