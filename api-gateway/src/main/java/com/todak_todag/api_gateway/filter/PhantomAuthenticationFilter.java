eb117adb3d6da3a4f47c7f9b4b9af52b95afaec9
Seo-JS0823 <seong0093571@naver.com>
2026-09-01 21:50:17 +0900

    feat: 글로벌 필터가 헤더값 덮어쓰고 넣어주기

--- api-gateway/src/main/java/com/todak_todag/api_gateway/filter/PhantomAuthenticationFilter.java
+++ api-gateway/src/main/java/com/todak_todag/api_gateway/filter/PhantomAuthenticationFilter.java
@@ -3,8 +3,12 @@ package com.todak_todag.api_gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
+import com.todak_todag.api_gateway.authentication.ClientContext;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -12,8 +16,8 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class PhantomAuthenticationFilter implements GlobalFilter, Ordered {
-	
-	private static final String USER_ID_HEADER = "X-User-Id";
+
+  private static final String USER_ID_HEADER = "X-User-Id";
 	
 	private static final String USER_ROLE_HEADER = "X-User-Role";
 	
@@ -24,8 +28,47 @@ public class PhantomAuthenticationFilter implements GlobalFilter, Ordered {
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerWebExchange sanitizedExchange = removeClientHeaders(exchange);
 		
-		return chain.filter(exchange);
+		return sanitizedExchange.getPrincipal()
+				.filter(Authentication.class::isInstance)
+				.cast(Authentication.class)
+				.filter(Authentication::isAuthenticated)
+				.map(Authentication::getPrincipal)
+				.filter(ClientContext.class::isInstance)
+				.cast(ClientContext.class)
+				.flatMap(clientContext -> chain
+						.filter(addClientHeaders(sanitizedExchange, clientContext))
+				)
+				.switchIfEmpty(Mono.defer(() -> chain.filter(sanitizedExchange)));
+	}
+	
+	private ServerWebExchange addClientHeaders(ServerWebExchange sanitizedExchange, ClientContext clientContext) {
+		ServerHttpRequest request = sanitizedExchange.getRequest().mutate()
+				.headers(headers -> {
+					headers.set(USER_ID_HEADER, clientContext.userId());
+					
+					headers.set(USER_ROLE_HEADER, clientContext.role());
+				})
+				.build();
+		
+		return sanitizedExchange.mutate()
+				.request(request)
+				.build();
+	}
+
+	private ServerWebExchange removeClientHeaders(ServerWebExchange exchange) {
+		ServerHttpRequest request = exchange.getRequest()
+				.mutate()
+				.headers(headers -> {
+					headers.remove(USER_ID_HEADER);
+					headers.remove(USER_ROLE_HEADER);
+				})
+				.build();
+		
+		return exchange.mutate()
+				.request(request)
+				.build();
 	}
 
 }
