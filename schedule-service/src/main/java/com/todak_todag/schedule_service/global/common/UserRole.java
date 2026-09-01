2116e88784af9308f2e05a2ea130dd8756233c3e
rvbear <rvbear2000@gmail.com>
2026-09-01 20:54:47 +0900

    feat: Spring Security 공통 코드 설정

---
+++ schedule-service/src/main/java/com/todak_todag/schedule_service/global/common/UserRole.java
@@ -0,0 +1,11 @@
+package com.todak_todag.schedule_service.global.common;
+
+public enum UserRole {
+
+    PATIENT,
+    HOSPITAL_STAFF,
+    SERVICE_PROVIDER,
+    SOCIAL_WORKER,
+    ADMIN,
+    MASTER
+}
---
+++ schedule-service/src/main/java/com/todak_todag/schedule_service/global/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.todak_todag.schedule_service.global.config;
+
+import com.todak_todag.schedule_service.global.security.HeaderAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // Gateway Header -> UserContext 로 파싱
+                // Controller 에서는 @AuthenticationPrincipal UserContext user 로 사용가능합니다.
+                // ROLE 접두사가 붙습니다.
+                // @PreAuthorize("hasRole('MASTER')") 로 Controller 에서 사용할 수 있습니다.
+                // 여러가지의 경우 @PreAuthorize("hasAnyRole('MASTER', 'ADMIN')") 으로 사용할 수 있습니다.
+                .addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        // 공개 API
+                        .requestMatchers(
+                                "/api/v1/service-results/**",
+                                "/api/v1/service-schedules/**",
+                                "/actuator/**",
+                                "/swagger-ui/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
