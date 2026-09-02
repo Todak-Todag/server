4855733a13447d74f30946510bfe4d5efcbe70f4
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-02 23:43:22 +0900

    test: 요청자가 Care Plan의 환자가 아니면 예외

--- care-plan-service/src/main/java/com/spring/careplanservice/careplan/application/service/command/ServicePreferenceCommandService.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/careplan/application/service/command/ServicePreferenceCommandService.java
@@ -16,8 +16,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 public class ServicePreferenceCommandService {
