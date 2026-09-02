f912e4926820e5bd987fd343d7f15ac93723fba9
Seo-JS0823 <seong0093571@naver.com>
2026-09-02 14:55:57 +0900

    test: Internal API Interceptor 통합 테스트 작성

--- user-service/src/test/java/com/todak_todag/user_service/controller/InternalApiSecurityIntegrationTest.java
+++ user-service/src/test/java/com/todak_todag/user_service/controller/InternalApiSecurityIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.todak_todag.user_service.controller;
 
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todak_todag.user_service.global.exception.CommonErrorCode;
 import com.todak_todag.user_service.global.security.InternalHeader;
 
 @SpringBootTest
@@ -23,6 +26,10 @@ public class InternalApiSecurityIntegrationTest {
 
 	private static final String INTERNAL_API_KEY = "01234567890123456789012345678901";
 	
+	private static final String INTERNAL_TEST_URL = "/internal/v1/probe";
+	
+	private static final String ERROR_CODE = CommonErrorCode.UNAUTHORIZED_INTERNAL_REQUEST.getCode();
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
@@ -30,7 +37,7 @@ public class InternalApiSecurityIntegrationTest {
 	static class TestInternalEndPoint {
 		
 		@RestController
-		@RequestMapping("/internal/v1/probe")
+		@RequestMapping(INTERNAL_TEST_URL)
 		static class ProbeController {
 			
 			@GetMapping
@@ -46,10 +53,40 @@ public class InternalApiSecurityIntegrationTest {
 	@DisplayName("유효한 내부 키로 호출하면 통과")
 	void internalApiTest_1() throws Exception {
 		mockMvc.perform(
-				get("/internal/v1/probe")
+				get(INTERNAL_TEST_URL)
 				.header(InternalHeader.INTERNAL_API_KEY, INTERNAL_API_KEY)
 		)
 		.andExpect(status().isOk());
+	}
+	
+	@Test
+	@DisplayName("내부 키 없이 호출하면 401 응답")
+	void internalApiTest_fail_1() throws Exception {
+		mockMvc.perform(
+				get(INTERNAL_TEST_URL)
+		)
+		.andExpect(status().isUnauthorized())
+		.andExpect(jsonPath("$.success").value(false))
+		.andExpect(jsonPath("$.error.errorCode").value(ERROR_CODE));
+	}
+	
+	@Test
+	@DisplayName("잘못된 내부 키로 호출하면 401 응답")
+	void internalApiTest_fail_2() throws Exception {
+		mockMvc.perform(
+				get(INTERNAL_TEST_URL)
+				.header(InternalHeader.INTERNAL_API_KEY, "fail_internal_test_key")
+		)
+		.andExpect(status().isUnauthorized())
+		.andExpect(jsonPath("$.success").value(false))
+		.andExpect(jsonPath("$.error.errorCode").value(ERROR_CODE));
+	}
+	
+	@Test
+	@DisplayName("공개 API는 내부 키 없이도 인터셉터를 거치지 않아야함")
+	void internalApiTest_isPublicApi() throws Exception {
+		mockMvc.perform(get("/api/v1/probe"))
+		.andExpect(status().is(not(401)));
 	}
 	
 }
