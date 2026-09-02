ed73e968a5f901abc53103b1e15be1e535d15f2f
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-02 18:42:47 +0900

    feat: Care Plan 서비스 선택 API 구현

--- care-plan-service/src/main/java/com/spring/careplanservice/careplan/presentation/controller/api/CarePlanServiceController.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/careplan/presentation/controller/api/CarePlanServiceController.java
@@ -1,11 +1,15 @@
 package com.spring.careplanservice.careplan.presentation.controller.api;
 
+import com.spring.careplanservice.careplan.application.command.CarePlanServiceSelectCommand;
+import com.spring.careplanservice.careplan.application.result.CarePlanServiceSelectResult;
+import com.spring.careplanservice.careplan.application.service.command.CarePlanServiceCommandService;
 import com.spring.careplanservice.careplan.presentation.request.CarePlanServiceSelectRequest;
 import com.spring.careplanservice.careplan.presentation.response.CarePlanServiceSelectResponse;
 import com.spring.careplanservice.global.response.ApiResponse;
 import com.spring.careplanservice.global.security.UserContext;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +22,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class CarePlanServiceController {
+    private final CarePlanServiceCommandService carePlanServiceCommandService;
 
     @PreAuthorize("hasRole('PATIENT')")
     @PostMapping("/care-plans/{carePlanId}/services")
@@ -26,6 +31,25 @@ public class CarePlanServiceController {
             @PathVariable("carePlanId") UUID carePlanId,
             @Valid @RequestBody CarePlanServiceSelectRequest carePlanServiceSelectRequest
     ) {
-        return null;
+        CarePlanServiceSelectCommand carePlanServiceSelectCommand = carePlanServiceSelectRequest.toCommand(
+                user.userId(),
+                carePlanId
+        );
+
+        CarePlanServiceSelectResult carePlanServiceSelectResult = carePlanServiceCommandService.selectCarePlanService(
+                carePlanServiceSelectCommand
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        ApiResponse.success(
+                                HttpStatus.CREATED.value(),
+                                "Care Plan 서비스 선택 성공",
+                                CarePlanServiceSelectResponse.from(
+                                        carePlanServiceSelectResult
+                                )
+                        )
+                );
     }
 }
