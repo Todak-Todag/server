195d9c241d75ca19e31185a5e8cdaa5d801ba731
Seo-JS0823 <seong0093571@naver.com>
2026-09-03 03:09:01 +0900

    feat: 회원가입 컨트롤러 준비

--- user-service/src/main/java/com/todak_todag/user_service/user/presentation/api_controller/UserApiController.java
+++ user-service/src/main/java/com/todak_todag/user_service/user/presentation/api_controller/UserApiController.java
@@ -1,20 +1,30 @@
 package com.todak_todag.user_service.user.presentation.api_controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todak_todag.user_service.global.response.ApiResponse;
+import com.todak_todag.user_service.user.application.service.command.UserCreateService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-public class UserApiController {
+@Validated
+public class UserApiController implements UserApiSpec {
 
+	private final UserCreateService userCreateService;
+	
+	@Override
 	@PostMapping("/signup")
-	public ApiResponse<Object> userSignup() {
+	public ResponseEntity<ApiResponse<Object>> createUserSignup() {
+		
+		
+		
 		return null;
 	}
 }
---
+++ user-service/src/main/java/com/todak_todag/user_service/user/presentation/api_controller/UserApiSpec.java
@@ -0,0 +1,30 @@
+package com.todak_todag.user_service.user.presentation.api_controller;
+
+import org.springframework.http.ResponseEntity;
+
+import com.todak_todag.user_service.global.response.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Service User", description = "사용자 API")
+public interface UserApiSpec {
+
+	@Operation(
+			summary = "사용자 회원가입",
+			description = """
+					신규 사용자의 회원가입을 진행하고 필수 약관 동의 내역을 함께 저장한다.
+					
+					- username: 6자 이상 영문소문자로 시작 영문/숫자 조합
+					- password: 8자리 이상 영문/숫자/특수문자 각각 하나 이상 비밀번호는 해시후 저장
+					- name: 숫자, 특수문자, 공백 불가
+					
+					회원가입 후 사용자는 PENDING 상태로 저장되며
+					관리자 또는 운영자의 승인을 받아야한다.
+			"""
+	)
+	@ApiResponses
+	ResponseEntity<ApiResponse<Object>> createUserSignup();
+	
+}
