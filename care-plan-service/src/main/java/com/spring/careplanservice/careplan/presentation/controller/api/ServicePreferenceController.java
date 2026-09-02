f4452ef63718ea95180d3391b0836d090a5e29fb
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-02 23:29:59 +0900

    feat: 서비스 희망 일정 생성 API 구현

--- care-plan-service/src/main/java/com/spring/careplanservice/careplan/presentation/controller/api/ServicePreferenceController.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/careplan/presentation/controller/api/ServicePreferenceController.java
@@ -1,4 +1,59 @@
 package com.spring.careplanservice.careplan.presentation.controller.api;
 
+
+import com.spring.careplanservice.careplan.application.command.ServicePreferenceCreateCommand;
+import com.spring.careplanservice.careplan.application.result.ServicePreferenceCreateResult;
+import com.spring.careplanservice.careplan.application.service.command.ServicePreferenceCommandService;
+import com.spring.careplanservice.careplan.presentation.request.ServicePreferenceCreateRequest;
+import com.spring.careplanservice.careplan.presentation.response.ServicePreferenceCreateResponse;
+import com.spring.careplanservice.global.response.ApiResponse;
+import com.spring.careplanservice.global.security.UserContext;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ServicePreferenceController {
+    private final ServicePreferenceCommandService servicePreferenceCommandService;
+
+    @PreAuthorize("hasRole('PATIENT')")
+    @PostMapping(
+            "/care-plan-services/{planServiceId}/service-preferences"
+    )
+    public ResponseEntity<ApiResponse<ServicePreferenceCreateResponse>>
+    createServicePreference(
+            @AuthenticationPrincipal UserContext user,
+            @PathVariable("planServiceId") UUID planServiceId,
+            @Valid @RequestBody
+            ServicePreferenceCreateRequest servicePreferenceCreateRequest
+    ) {
+
+        ServicePreferenceCreateCommand servicePreferenceCreateCommand =
+                servicePreferenceCreateRequest.toCommand(
+                        user.userId(),
+                        planServiceId
+                );
+
+        ServicePreferenceCreateResult servicePreferenceCreateResult = servicePreferenceCommandService.createServicePreference(servicePreferenceCreateCommand);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        ApiResponse.success(
+                                HttpStatus.CREATED.value(),
+                                "서비스 희망 일정 등록 성공",
+                                ServicePreferenceCreateResponse.from(
+                                        servicePreferenceCreateResult
+                                )
+                        )
+                );
+    }
 }
