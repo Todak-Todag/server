4ed240a5a777c0d5f494ec84e12ec32420423b9a
summmmmmmin <jsoomin01@naver.com>
2026-09-03 16:58:10 +0900

    feat: 서비스 종류 저장 및 서비스명 중복 조회 리포지토리 추가

--- provider-service/src/main/java/com/todak_todag/provider_service/provider/domain/repository/query/ProvideServiceQueryRepository.java
+++ provider-service/src/main/java/com/todak_todag/provider_service/provider/domain/repository/query/ProvideServiceQueryRepository.java
@@ -5,4 +5,6 @@ import java.util.UUID;
 public interface ProvideServiceQueryRepository {
 
     boolean existsById(UUID provideServiceId);
+
+    boolean existsByName(String name);
 }
--- provider-service/src/main/java/com/todak_todag/provider_service/provider/infrastructure/adapter/ProvideServicePersistenceAdapter.java
+++ provider-service/src/main/java/com/todak_todag/provider_service/provider/infrastructure/adapter/ProvideServicePersistenceAdapter.java
@@ -1,5 +1,7 @@
 package com.todak_todag.provider_service.provider.infrastructure.adapter;
 
+import com.todak_todag.provider_service.provider.domain.entity.ProvideService;
+import com.todak_todag.provider_service.provider.domain.repository.command.ProvideServiceCommandRepository;
 import com.todak_todag.provider_service.provider.domain.repository.query.ProvideServiceQueryRepository;
 import com.todak_todag.provider_service.provider.infrastructure.persistence.JpaProvideServiceRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +11,23 @@ import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
-public class ProvideServicePersistenceAdapter implements ProvideServiceQueryRepository {
+public class ProvideServicePersistenceAdapter
+        implements ProvideServiceQueryRepository, ProvideServiceCommandRepository {
 
     private final JpaProvideServiceRepository jpaProvideServiceRepository;
 
     @Override
+    public ProvideService save(ProvideService provideService) {
+        return jpaProvideServiceRepository.save(provideService);
+    }
+
+    @Override
     public boolean existsById(UUID provideServiceId) {
         return jpaProvideServiceRepository.existsById(provideServiceId);
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return jpaProvideServiceRepository.existsByName(name);
     }
 }
