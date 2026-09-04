c0f668bb072d875ae3f6caa6451e2c9f8ece03f1
Seo-JS0823 <seong0093571@naver.com>
2026-09-04 23:01:51 +0900

    feat: 내부 API Controller 생성

--- user-service/src/main/java/com/todak_todag/user_service/user/application/service/query/UserQueryService.java
+++ user-service/src/main/java/com/todak_todag/user_service/user/application/service/query/UserQueryService.java
@@ -1,5 +1,16 @@
 package com.todak_todag.user_service.user.application.service.query;
 
+import com.todak_todag.user_service.user.domain.repository.query.UserQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserQueryService {
 
+    private final UserQueryRepository userQueryRepository;
+
+    
 }
--- user-service/src/main/java/com/todak_todag/user_service/user/presentation/controller/internal/UserInternalController.java
+++ user-service/src/main/java/com/todak_todag/user_service/user/presentation/controller/internal/UserInternalController.java
@@ -1,5 +1,29 @@
 package com.todak_todag.user_service.user.presentation.controller.internal;
 
-public class UserInternalController {
+import com.todak_todag.user_service.global.response.ApiResponse;
+import com.todak_todag.user_service.user.application.service.query.UserQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/v1/users")
+public class UserInternalController implements UserInternalSpec {
+
+    private UserQueryService userQueryService;
+
+    @Override
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse<Void>> readUser(
+            @PathVariable("userId") UUID userId
+    ) {
+
+        return null;
+    }
 }
