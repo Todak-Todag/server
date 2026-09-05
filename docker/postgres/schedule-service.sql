ee7a6463b7558bc2c82725089f64c893568fd517
rvbear <rvbear2000@gmail.com>
2026-09-06 02:00:39 +0900

    feat: schedule-service.sql 최신화 - p_service_matching_attempts Table 추가 - 논리 FK 주석 추가

--- docker/postgres/schedule-service.sql
+++ docker/postgres/schedule-service.sql
@@ -30,7 +30,10 @@ CREATE TABLE IF NOT EXISTS schedule_schema.p_service_schedules (
 
 CREATE TABLE IF NOT EXISTS schedule_schema.p_care_plan_service_results (
     service_result_id UUID PRIMARY KEY,
+
+    -- 논리 FK -> schedule_schema.p_service_schedules(service_schedule_id)
     service_schedule_id UUID NOT NULL,
+
     started_at TIMESTAMP NOT NULL,
     finished_at TIMESTAMP NOT NULL,
     note TEXT,
@@ -59,4 +62,38 @@ CREATE TABLE IF NOT EXISTS schedule_schema.p_schedule_outbox_events (
     created_by UUID NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
     updated_by UUID NOT NULL
+);
+
+CREATE TYPE schedule_schema.service_matching_attempts_status AS ENUM (
+    'MATCHED', 'FAILED'
+);
+
+CREATE TABLE schedule_schema.p_service_matching_attempts (
+    matching_attempt_id UUID NOT NULL,
+
+    -- 논리 FK -> care_plan_schema.p_care_plans(care_plan_id)
+    care_plan_id UUID NOT NULL,
+
+    -- 논리 FK -> user_schema.p_regions(region_id)
+    region_id UUID NOT NULL,
+
+    -- 논리 FK -> provider_schema.p_provide_services(provide_service_id)
+    provide_service_id UUID NOT NULL,
+
+    -- 논리 FK -> care_plan_schema.p_care_plan_service_preferences(service_preference_id)
+    service_preference_id UUID NOT NULL,
+
+    -- 논리 FK -> provider_schema.p_provide_service_offerings(service_offering_id)
+    service_offering_id UUID NOT NULL,
+
+    status schedule_schema.service_matching_attempts_status NOT NULL,
+    failure_reason TEXT,
+    matched_at TIMESTAMP,
+    failed_at TIMESTAMP,
+    created_at TIMESTAMPTZ NOT NULL,
+    created_by UUID NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+    updated_by UUID NOT NULL,
+    deleted_at TIMESTAMPTZ,
+    deleted_by UUID
 );
