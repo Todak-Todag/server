6a0d4a19c23478feaea4a279f27d72bcfc0bd9cc
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-07 03:53:18 +0900

    chore: TODO 제거

--- care-plan-service/src/main/java/com/spring/careplanservice/careplan/application/service/command/CarePlanCommandService.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/careplan/application/service/command/CarePlanCommandService.java
@@ -119,7 +119,6 @@ public class CarePlanCommandService {
                 carePlanStatusUpdateCommand.status()
         );
 
-        // TODO: User-Service의 구현/머지 후 실제 연동 확인
         if (carePlan.getStatus() == CarePlanStatus.CONFIRMED) {
             UserFindResult userFindResult = userQueryPort.findById(
                     carePlan.getPatientId()
