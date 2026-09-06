c86f8bcedeafb356cedfc32dfb1caf0e3153fb43
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-06 20:43:37 +0900

    feat: Swagger X-User-Id, X-User-Role 인증 설정 추가

---
+++ care-plan-service/src/main/java/com/spring/careplanservice/global/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.spring.careplanservice.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "X-User-Id",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-User-Id")
+                        )
+                        .addSecuritySchemes(
+                                "X-User-Role",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-User-Role")
+                        )
+                );
+    }
+}
