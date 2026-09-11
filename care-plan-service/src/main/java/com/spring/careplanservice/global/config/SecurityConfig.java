7f110961999143d8c80cccb78cf2f04d98fca3d2
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-11 19:19:31 +0900

    feat: Prometheus Grafana Loki Zipkin 모니터링 구성

--- care-plan-service/src/main/java/com/spring/careplanservice/global/config/SecurityConfig.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/global/config/SecurityConfig.java
@@ -41,11 +41,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/internal/**",
-                                "/actuator/health/**",
                                 "/v3/**",
                                 "/swagger-ui/**",
-                                "/actuator/prometheus",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/health/**",
+                                "/actuator/info",
+                                "/actuator/prometheus"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
