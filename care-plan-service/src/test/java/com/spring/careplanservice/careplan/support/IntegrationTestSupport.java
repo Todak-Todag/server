b6d89585de746a2a5158e0b56592fceadbee4387
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-13 20:48:26 +0900

    test: Spring Context 재사용에 맞춰 Testcontainers 공유 방식 수정

--- care-plan-service/src/test/java/com/spring/careplanservice/careplan/support/IntegrationTestSupport.java
+++ care-plan-service/src/test/java/com/spring/careplanservice/careplan/support/IntegrationTestSupport.java
@@ -4,35 +4,40 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.rabbitmq.RabbitMQContainer;
 
-@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class IntegrationTestSupport {
-    @Container
-    static final PostgreSQLContainer<?> POSTGRES =
-            new PostgreSQLContainer<>("postgres:17-alpine")
-                    .withDatabaseName("todaktodag_test")
-                    .withUsername("postgres")
-                    .withPassword("password");
-
-    @Container
-    static final RabbitMQContainer RABBIT_MQ =
-            new RabbitMQContainer("rabbitmq:4-alpine");
-
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
+        registry.add(
+                "spring.datasource.url",
+                TestContainers.POSTGRES::getJdbcUrl
+        );
+        registry.add(
+                "spring.datasource.username",
+                TestContainers.POSTGRES::getUsername
+        );
+        registry.add(
+                "spring.datasource.password",
+                TestContainers.POSTGRES::getPassword
+        );
 
-        registry.add("spring.rabbitmq.host", RABBIT_MQ::getHost);
-        registry.add("spring.rabbitmq.port", RABBIT_MQ::getAmqpPort);
-        registry.add("spring.rabbitmq.username", RABBIT_MQ::getAdminUsername);
-        registry.add("spring.rabbitmq.password", RABBIT_MQ::getAdminPassword);
+        registry.add(
+                "spring.rabbitmq.host",
+                TestContainers.RABBIT_MQ::getHost
+        );
+        registry.add(
+                "spring.rabbitmq.port",
+                TestContainers.RABBIT_MQ::getAmqpPort
+        );
+        registry.add(
+                "spring.rabbitmq.username",
+                TestContainers.RABBIT_MQ::getAdminUsername
+        );
+        registry.add(
+                "spring.rabbitmq.password",
+                TestContainers.RABBIT_MQ::getAdminPassword
+        );
     }
 }
