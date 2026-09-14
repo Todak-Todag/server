34cc461707efc1e8406c772707f67f5b8b30066d
Jehee Won <jehee0076@gmail.com>
2026-09-14 15:15:39 +0900

    fix: Discharge 테이블명 정합성 수정

--- docker/postgres/discharge-service.sql
+++ docker/postgres/discharge-service.sql
@@ -4,13 +4,13 @@ CREATE TYPE discharge_schema.discharge_status AS ENUM (
     'SCHEDULED', 'POSTPONED', 'COMPLETED', 'CANCELED'
 );
 
-CREATE TABLE IF NOT EXISTS discharge_schema.p_discharge (
-    discharge_id UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS discharge_schema.p_discharges (
+                                                             discharge_id UUID PRIMARY KEY,
     -- 논리 FK -> user_schema.p_users(user_id)
-    patient_id UUID NOT NULL,
+                                                             patient_id UUID NOT NULL,
     -- 논리 FK -> user_schema.p_users(user_id)
-    hospital_staff_id UUID NOT NULL,
-    hospital_name VARCHAR(50) NOT NULL,
+                                                             hospital_staff_id UUID NOT NULL,
+                                                             hospital_name VARCHAR(50) NOT NULL,
     schedule_date DATE,
     actual_date DATE,
     status discharge_schema.discharge_status NOT NULL DEFAULT 'SCHEDULED',
@@ -20,4 +20,4 @@ CREATE TABLE IF NOT EXISTS discharge_schema.p_discharge (
     updated_by UUID NOT NULL,
     deleted_at TIMESTAMPTZ,
     deleted_by UUID
-);
+    );
