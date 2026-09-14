0f685ee3371af10282ffbb040e03843ef5f22b4a
summmmmmmin <jsoomin01@naver.com>
2026-09-14 22:53:23 +0900

    feat: 공통 에러 코드 NOT_FOUND, METHOD_NOT_ALLOWED 추가

--- provider-service/src/main/java/com/todak_todag/provider_service/global/exception/CommonErrorCode.java
+++ provider-service/src/main/java/com/todak_todag/provider_service/global/exception/CommonErrorCode.java
@@ -10,6 +10,8 @@ public enum CommonErrorCode implements ErrorCode {
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 올바르지 않습니다."),
     UNAUTHORIZED_INTERNAL_REQUEST(HttpStatus.UNAUTHORIZED, "잘못된 접근입니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 경로를 찾을 수 없습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
