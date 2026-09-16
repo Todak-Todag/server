0939503fbde31b5f274614287736c473582a1830
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-16 11:03:29 +0900

    feat: baseEntity & yml 수정

--- care-plan-service/src/main/java/com/spring/careplanservice/global/common/BaseUpdatableEntity.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/global/common/BaseUpdatableEntity.java
@@ -1,18 +1,20 @@
 package com.spring.careplanservice.global.common;
 
-
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
 @MappedSuperclass
-public abstract class BaseAuditEntity extends BaseCreateDeleteEntity {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseUpdatableEntity extends BaseEntity {
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
