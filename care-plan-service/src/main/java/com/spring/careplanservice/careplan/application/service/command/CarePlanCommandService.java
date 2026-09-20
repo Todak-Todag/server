9c2d1fe4e012e3d9b22f9d3b4de1836f817f792b
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-21 01:06:42 +0900

    feat: CarePlanConfirmed 이벤트 식별자 추가

--- care-plan-service/src/main/java/com/spring/careplanservice/careplan/application/service/command/CarePlanCommandService.java
+++ care-plan-service/src/main/java/com/spring/careplanservice/careplan/application/service/command/CarePlanCommandService.java
@@ -279,6 +279,7 @@ public class CarePlanCommandService {
         );
 
         return new CarePlanConfirmedEvent(
+                UUID.randomUUID(),
                 carePlanId,
                 regionId,
                 services
